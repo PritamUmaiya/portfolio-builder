b184e5c7851e81158080fa2fc76d44b77dc72602
Pritam Jaiswal <pritamjaiswal.me@gmail.com>
2024-09-13 05:18:46 +0000

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,28 +1,86 @@
 CREATE TABLE user (
+    id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT,
     email TEXT NOT NULL,
-    password TEXT NOT NULL, -- Hashed
+    password TEXT NOT NULL,  -- Hashed
     avatar TEXT,
     bio TEXT,
     profession TEXT,
-    public_email TEXT, -- Visible in profile
-    secondary_btn TEXT NOT NULL  -- (cv/resume/contact)
+    works_at TEXT,
+    studing_in TEXT,
+    lives_in TEXT,
+    public_email TEXT,  -- Visible in profile
+    phone TEXT,
+    link TEXT,
+    about TEXT,
+    secondary_btn TEXT NOT NULL DEFAULT 'contact',  -- (cv/resume/contact)
+    secondary_btn_link TEXT NOT NULL DEFAULT '#contact',
+    -- Social Media & links
+    facebook TEXT,
+    instagram TEXT,
+    threads TEXT,
+    twitter_x TEXT,
+    linkedin TEXT,
+    github TEXT,
+    gitlab TEXT,
+    youtube TEXT,
+    twitch TEXT,
+    medium TEXT,
+    behance TEXT,
+    dribble TEXT
 );
-
-CREATE TABLE links (
-    id INTEGER NOT NULL PRIMARY KEY,
-    site TEXT NOT NULL,
-    url TEXT NOT NULL
-);
-
-CREATE INDEX idx_links_id ON links(id);
 
 CREATE TABLE portfolio (
-    id INTEGER NOT NULL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     image TEXT NOT NULL,
     title TEXT,
-    description TEXT
+    description TEXT,
+    link TEXT
 );
 
-CREATE INDEX idx_portfolio_id ON portfolio(id);
+CREATE TABLE education (
+    id INTEGER PRIMARY KEY,
+    qualification TEXT NOT NULL,
+    school TEXT,
+    start_date TEXT,
+    end_date TEXT
+);
+
+CREATE TABLE experiences (
+    id INTEGER PRIMARY KEY,
+    work TEXT NOT NULL,
+    work_place TEXT,
+    start_date TEXT,
+    end_date TEXT
+);
+
+CREATE TABLE achievements (
+    id INTEGER PRIMARY KEY,
+    achievement TEXT NOT NULL,
+    associated TEXT,
+    year TEXT
+);
+
+CREATE TABLE certifications (
+    id INTEGER PRIMARY KEY,
+    certification TEXT NOT NULL,
+    associated TEXT,
+    year TEXT,
+    credential TEXT,
+    link TEXT
+);
+
+CREATE TABLE skills (
+    id INTEGER PRIMARY KEY,
+    skill TEXT NOT NULL
+);
+
+CREATE TABLE inbox (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT,
+    email TEXT NOT NULL,
+    subject TEXT NOT NULL,
+    message TEXT NOT NULL
+);
