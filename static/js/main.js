9cdccf7d33d8a7dccbf61ae0c692eee02644dca7
Pritam Jaiswal <pritamjaiswal.me@gmail.com>
2024-09-15 04:11:35 +0000

    adding sidenav

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+// Function to toggle menu
+function toggleMenu() {
+    const sideNav = document.getElementById('sideNav');
+    const overlay = document.getElementById('overlay');
+    sideNav.classList.toggle('left-0');
+    overlay.classList.toggle('d-block');
+}
